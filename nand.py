a90c01ee91808b321c188431dad2c9c89d60193a
Eric Smith <spacewar@gmail.com>
2017-07-26 01:18:46 -0600

    Debug in NAND class uses its own debug variables, rather than Signal's.

--- nand.py
+++ nand.py
@@ -6,13 +6,23 @@ from component import Component
 
 class NAND(Component):
     __serial__ = 0
-    
+    __sc_debug__ = False
+
+    @classmethod
+    def set_global_debug(cls, value):
+        cls.__sc_debug__ = value
+
+    @classmethod
+    def get_global_debug(cls):
+        return cls.__sc_debug__
+
     def __init__(self, scheduler,
                  inputs,
                  output,
                  name = None,
                  prop_delay = 0):
         super().__init__(scheduler)
+        self.debug = False
         self.inputs = [i.bind(self) for i in inputs]
         if output is None:
             output = Signal(scheduler)
@@ -23,9 +33,24 @@ class NAND(Component):
         self.name = name
         self.prop_delay = prop_delay
 
+    def set_debug(self, value):
+        self.debug = value
+
+    def get_debug(self):
+        return self.debug
+
     def update(self):
-        #print(self.name, " update")
-        self.output.set(any (input.get() == 0 for input in self.inputs),
+        if any (input.get() == 0 for input in self.inputs):
+            new_val = 1
+        else:
+            new_val = 0
+        if NAND.__sc_debug__ or self.debug:
+            print("%s: updating %s to %d" %(self.name, self.output.name, new_val))
+            print("listeners:")
+            for l in self.output.components:
+                print(" %s" % l.name, end = '')
+            print()
+        self.output.set(new_val,
                         delay = self.prop_delay)
 
 if __name__ == '__main__':
